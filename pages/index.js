3b9fa3c2e935165a83aa3fd75e92332af54d4942
Zeeshan Haider <zeeshanhaider1902@gmail.com>
2023-11-21 12:33:35 +0500

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+// pages/index.js
+import { useEffect, useState } from 'react';
+import { format } from 'date-fns';
+
+export default function Home() {
+  const [message, setMessage] = useState('');
+  const [dateTime, setDateTime] = useState('');
+
+  useEffect(() => {
+    fetch('http://192.168.49.2:30001/api/hello')
+      .then(response => response.json())
+      .then(data => {
+        setMessage(data.message);
+        // Use date-fns to format the date and time
+        const now = new Date();
+        const formattedDateTime = format(now, 'MM/dd/yyyy HH:mm');
+        setDateTime(formattedDateTime);
+      });
+  }, []);
+
+  return (
+    <div>
+      <p>{dateTime && message ? `${dateTime} ${message}` : 'Loading...'}</p>
+    </div>
+  );
+}
